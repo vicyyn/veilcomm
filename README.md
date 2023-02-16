86f0d59ff3b4b4dd23945adcc8f58f5c70ea6a27
vicyyn <nader_ouerdiane@hotmail.com>
2023-02-16 11:38:52 +0100

    base socket communication

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 Onion Routing Messaging Service
+
+### Node Discovery
+
+DHT routing Implementation using Kademlia.
+
+- [ ] What to Store (Key/Value Pair)
+
+- [ ] Node Structure
+
+- [ ] Hash Function
+
+- [ ] Nodes Communication (TCP/UDP)
+
+- [ ] Implement the Routing Table (Add/Remove Nodes and Find k Closest Nodes)
+
+- [ ] Implement Iterative Lookup and Handling Unresponsive Nodes
+
+Possible ready to use implementations
+
+`rust-libp2p`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: ./program server|client");
+        std::process::exit(1);
+    }
+
+    match args[1].as_str() {
+        "server" => run_server(),
+        "client" => run_client(),
+        _ => {
+            eprintln!("Invalid argument. Usage: ./program server|client");
+            std::process::exit(1);
+        }
+    }
+}
+
+fn run_server() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                let mut buffer = [0; 512];
+                stream.read(&mut buffer)?;
+
+                let message = String::from_utf8_lossy(&buffer[..]);
+                println!("Received message: {}", message);
+
+                stream.write_all(b"Message received.")?;
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn run_client() -> std::io::Result<()> {
+    let mut stream = TcpStream::connect("127.0.0.1:8080")?;
+
+    let message = "Hello, server!";
+    stream.write_all(message.as_bytes())?;
+
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer)?;
+
+    let message = String::from_utf8_lossy(&buffer[..]);
+    println!("Received message: {}", message);
+
+    Ok(())
 }
