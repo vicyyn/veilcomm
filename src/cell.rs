ff0ee38192ab7bd06cc717d2e4f369d434b0bcd1
vicyyn <nader_ouerdiane@hotmail.com>
2023-02-20 22:07:53 +0100

    connection

--- src/cell.rs
+++ src/cell.rs
@@ -1,10 +1,25 @@
 // The basic unit of communication for onion routers and onion
 // proxies is a fixed-width "cell". 512 bytes size.
 
+use serde::{Deserialize, Serialize};
+use serde_big_array::BigArray;
+
 const CELL_PAYLOAD_SIZE: usize = 509;
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Cell {
     pub circ_id: u16,
     pub command: u8,
+    #[serde(with = "BigArray")]
     pub payload: [u8; CELL_PAYLOAD_SIZE],
+}
+
+impl Default for Cell {
+    fn default() -> Self {
+        Self {
+            circ_id: 0,
+            command: 0,
+            payload: [0; CELL_PAYLOAD_SIZE],
+        }
+    }
 }
--- src/connection.rs
+++ src/connection.rs
@@ -1,14 +1,72 @@
 // UDP connection with another node in the network.
 use crate::*;
-use std::collections::{BinaryHeap, HashMap, HashSet};
-use std::fmt::{Binary, Debug, Error, Formatter};
+use std::collections::HashMap;
 use std::net::UdpSocket;
+use std::str;
 use std::sync::{mpsc, Arc, Mutex};
-
-const CELL_PAYLOAD_SIZE: usize = 509;
+use std::thread;
 
 pub struct Connection {
     pub socket: Arc<UdpSocket>,
-    pub pending: Arc<Mutex<HashMap<Key, mpsc::Sender<Option<Response>>>>>,
+    pub pending: Arc<Mutex<HashMap<Key, mpsc::Sender<Option<Cell>>>>>,
     pub node: Node,
+}
+
+impl Connection {
+    pub fn new(node: Node) -> Self {
+        let socket = UdpSocket::bind(node.get_addr())
+            .expect("[FAILED] Rpc::new --> Error while binding UdpSocket to specified addr");
+
+        Self {
+            socket: Arc::new(socket),
+            pending: Arc::new(Mutex::new(HashMap::new())),
+            node,
+        }
+    }
+
+    pub fn open(connection: Connection) {
+        thread::spawn(move || {
+            let mut buf = [0u8; 512];
+
+            loop {
+                let (len, src_addr) = connection
+                    .socket
+                    .recv_from(&mut buf)
+                    .expect("[FAILED] Rpc::open --> Failed to receive data from peer");
+
+                let mut decoded: Cell = bincode::deserialize(&buf.to_vec())
+                    .expect("[FAILED] Rpc::open, serde_json --> Unable to decode string payload");
+
+                println!("{:?}", decoded);
+            }
+        });
+    }
+
+    pub fn send_cell(&self, cell: &Cell, destination: Node) {
+        let encoded = bincode::serialize(cell)
+            .expect("[FAILED] Rpc::send_msg --> Unable to serialize message");
+
+        self.socket
+            .send_to(&encoded, destination.get_addr())
+            .expect("[FAILED] Rpc::send_msg --> Error while sending message to specified address");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_connection() {
+        let node1 = Node::new("127.0.0.1".to_string(), 7999);
+        let node2 = Node::new("127.0.0.1".to_string(), 8000);
+
+        let connection1 = Connection::new(node1.clone());
+        let connection2 = Connection::new(node2.clone());
+
+        Connection::open(connection1);
+
+        connection2.send_cell(&Cell::default(), node1);
+        loop {}
+    }
 }
