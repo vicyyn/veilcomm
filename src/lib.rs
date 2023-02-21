6a86831dfb9cb7a5824503659091eb7fe0b75865
vicyyn <nader_ouerdiane@hotmail.com>
2023-02-21 12:04:53 +0100

    relay command

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 pub mod cell;
-pub mod command;
+pub mod cell_command;
 pub mod connection;
 pub mod key;
 pub mod node;
 pub mod payload;
+pub mod relay_command;
 
 pub use cell::*;
-pub use command::*;
+pub use cell_command::*;
 pub use connection::*;
 pub use key::*;
 pub use node::*;
 pub use payload::*;
+pub use relay_command::*;
---
+++ src/relay_command.rs
@@ -0,0 +1,42 @@
+pub enum RelayCommand {
+    Begin = 1,
+    Data = 2,
+    End = 3,
+    Connected = 4,
+    SendMe = 5,
+    Extend = 6,
+    Extended = 7,
+    Truncate = 8,
+    Truncated = 9,
+    Drop = 10,
+    Resolve = 11,
+    Resolved = 12,
+    BeginDir = 13,
+    Extend2 = 14,
+    Extended2 = 15,
+}
+
+impl TryFrom<u8> for RelayCommand {
+    type Error = &'static str;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            1 => Ok(Self::Begin),
+            2 => Ok(Self::Data),
+            3 => Ok(Self::End),
+            4 => Ok(Self::Connected),
+            5 => Ok(Self::SendMe),
+            6 => Ok(Self::Extend),
+            7 => Ok(Self::Extended),
+            8 => Ok(Self::Truncate),
+            9 => Ok(Self::Truncated),
+            10 => Ok(Self::Drop),
+            11 => Ok(Self::Resolve),
+            12 => Ok(Self::Resolved),
+            13 => Ok(Self::BeginDir),
+            14 => Ok(Self::Extend),
+            15 => Ok(Self::Extend2),
+            _ => Err("unrecognized command"),
+        }
+    }
+}
