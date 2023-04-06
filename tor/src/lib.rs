21a8df83039fa040107606847240e4e5eda3643a
vicyyn <nader_ouerdiane@hotmail.com>
2023-04-06 22:14:28 +0100

    rendezvous point

--- tor/src/lib.rs
+++ tor/src/lib.rs
@@ -1,9 +1,11 @@
 pub mod circuit;
 pub mod circuit_node;
+pub mod hidden_user_descriptor;
 pub mod keys;
 pub mod pending_response;
 
 pub use circuit::*;
 pub use circuit_node::*;
+pub use hidden_user_descriptor::*;
 pub use keys::*;
 pub use pending_response::*;
---
+++ tor/src/rendezvous.rs
@@ -0,0 +1,11 @@
+// 1. create communication
+// generate publickey
+// choose introduction points -> advertise on lookup service
+// build circuit for each introduction point
+//
+// 2.
+// connect to lookup service
+// choose OR as rendezvous point (RP) and build the circuit
+// open a stream with one of the introduction points
+// send encrypted message containing the RP, rendezvous cookie, half of the dh handshake and information about the sender
+// if connection is accepted, build circuit to RP, send half of the dh handshake, hash of key
